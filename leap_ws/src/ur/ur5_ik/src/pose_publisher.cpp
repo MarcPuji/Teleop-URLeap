00d704401b25c710e558e9f5ed7fef3bcf999ffa
Marc <marcpuru@gmail.com>
2018-11-14 15:56:31 +0100

    Leap and UR working with static tf, need to modify tf with Leap topic

--- leap_ws/src/ur/ur5_ik/src/pose_publisher.cpp
+++ leap_ws/src/ur/ur5_ik/src/pose_publisher.cpp
@@ -13,19 +13,19 @@
 // Callback Function
 float leap_x, leap_y;
 void Leap_CB(const visualization_msgs::MarkerArray msg){
-	leap_x = msg.markers[0].pose.position.x;
-    leap_y = msg.markers[0].pose.position.y;
+	//leap_x = msg.markers[0].pose.position.x;
+    //leap_y = msg.markers[0].pose.position.y;
 }
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "PosePulbisher_node");
+    ros::init(argc, argv, "tfleap_publisher");
     ros::NodeHandle n;
 
     // Service Client Initializer
     ros::ServiceClient IKClient = n.serviceClient<ur5ik::UR5IK>("UR5IK");
     // Subscriber to TargetPose
-    ros::Subscriber LeapSubscriber = n.subscribe<visualization_msgs::MarkerArray>("leap_motion/visualization_marker_array", 10, Leap_CB);
+    ros::Subscriber LeapSubscriber = n.subscribe<visualization_msgs::MarkerArray>("leap_motion/visualization_marker_array", 1000, Leap_CB);
 
     //Create the request and response objects.
     ur5ik::UR5IK::Request req;
@@ -36,41 +36,40 @@ int main(int argc, char** argv)
     // Create the Stapmed Transform for the Leap Motion
     geometry_msgs::TransformStamped transformStamped;
 
-    ros::Rate rate(50);
+    ros::Rate loop_rate(60);
     ros::spinOnce();
 
     while(ros::ok()){
+    	// The initial position of ee_link is (0.69, 0.19, 0.295) so let's move from there
+    	// Apply the necessary modifications given the Leap Motion situation
+    	transformStamped.header.stamp = ros::Time::now();
+    	transformStamped.header.frame_id = "world";
+    	transformStamped.child_frame_id = "leap_ee";    
+    	transformStamped.transform.translation.x = 0.69; //+ leap_x;
+    	transformStamped.transform.translation.y = 0.19; //+ leap_y;
+    	transformStamped.transform.translation.z = 0.295;
+    	// The EndEffector needs to be pointing down all the time
+    	tf2::Quaternion q;
+    	q.setRPY(-3.1416, 0, 0);
+    	transformStamped.transform.rotation.x = q.x();
+    	transformStamped.transform.rotation.y = q.y();
+    	transformStamped.transform.rotation.z = q.z();
+    	transformStamped.transform.rotation.w = q.w();
+    	// Broadcast the Transform
+    	br.sendTransform(transformStamped);
 
-        // The initial position of ee_link is (0.69, 0.19, 0.295) so let's move from there
-        // Apply the necessary modifications given the Leap Motion situation
-        transformStamped.header.stamp = ros::Time::now();
-        transformStamped.header.frame_id = "world";
-        transformStamped.child_frame_id = "Leap";
-        transformStamped.transform.translation.x = 0.69; //+ leap_x;
-        transformStamped.transform.translation.y = 0.19; //+ leap_y;
-        transformStamped.transform.translation.z = 0.295;
-        // The EndEffector needs to be pointing down all the time
-        tf2::Quaternion q;
-        q.setRPY(-3.1416, 0, 0);
-        transformStamped.transform.rotation.x = q.x();
-        transformStamped.transform.rotation.y = q.y();
-        transformStamped.transform.rotation.z = q.z();
-        transformStamped.transform.rotation.w = q.w();
-        // Broadcast the Transform
-        br.sendTransform(transformStamped);
+    	// Build the request
+    	req.pose.position.x = transformStamped.transform.translation.x;
+    	req.pose.position.y = transformStamped.transform.translation.y;
+    	req.pose.position.z = transformStamped.transform.translation.z;
+    	req.pose.orientation = transformStamped.transform.rotation;
 
-        // Build the request
-        req.pose.position.x = transformStamped.transform.translation.x;
-        req.pose.position.y = transformStamped.transform.translation.y;
-        req.pose.position.z = transformStamped.transform.translation.z;
-        req.pose.orientation = transformStamped.transform.rotation;
-
-        // Execute the command if we press the "a" button with keyboard
-        int c = getchar();
-    	if(c == 'a'){
-    		ros::service::waitForService("UR5IK", ros::Duration(5));
-    		IKClient.call(req,resp);
-    	}
+    	// Execute the command if we press the "a" button with keyboard
+    	//int c = getchar();
+    	//if(c == 'a'){
+   		//	ros::service::waitForService("UR5IK", ros::Duration(5));
+    	//	IKClient.call(req,resp);
+    	//}
     	ros::spinOnce();
     }
     return 0;
